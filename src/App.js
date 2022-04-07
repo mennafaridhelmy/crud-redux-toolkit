3e2a8c9148f4687e11b67254fb5ee4a91340c3b4
menna <mennafaridhelmy@gmail.com>
2022-04-07 06:26:32 +0200

    Posts application name changed

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Posts from './components/Posts';
 function App() {
   return (
     <div className="App">
-      <h1 className='page-title'>CRUD APP USING REDUX TOOLKIT</h1>
+      <h1 className='page-title'>POSTS CRUD APP USING REDUX TOOLKIT</h1>
       
       <Posts />
     </div>
